ecb4cf1f41b9a89f7d218b81e058a3874cf9d73c
unknown <turko_v@pc-it-mo-7.mainoffice.dykun.local>
2015-08-27 11:49:57 +0300

    first steps

---
+++ README.md
@@ -0,0 +1,5 @@
+Bootstrap Tokenfield
+====================
+Create elegant taggable fields with copy/paste and keyboard support using Bootstrap Tokefield
+
+In progress. Do not use it!
---
+++ TokenfieldAsset.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace akavov\tokenfield;
+
+use yii\web\AssetBundle;
+
+class TokenfieldAsset extends AssetBundle {
+    public $sourcePath = '';
+
+	public $css = [
+        'css/bootstrap-tokenfield.min.css',
+    ];
+
+	public $js = [
+		'js/bootstrap-tokenfield.min.js',
+	];
+
+    public $depends = [
+        'yii\web\JqueryAsset'
+    ];
+
+	/**
+     * @inheritdoc
+     */
+    public function init() {
+        $this->sourcePath = __DIR__ . '/assets';
+        parent::init();
+    }
+}
